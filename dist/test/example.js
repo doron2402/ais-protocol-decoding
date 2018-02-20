020623e38a9040ee6c37cbb4a749bd52f0676e69
Doron Segal <doron2402@gmail.com>
2018-02-20 13:14:54 -0800

    ignore dist/

--- dist/test/example.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("../lib/index");
-var rawMsg = process.env.AIS || '!AIVDM,1,1,,A,13u?etPv2;0n:dDPwUM1U1Cb069D,0*24';
-var aisDecoder = new index_1.Decoder(rawMsg);
