645a5f61fe19f8060e3b8eb01ac3ca4be191e2c0
Doron Segal <doron2402@gmail.com>
2019-06-10 22:28:13 -0700

    adding tests and testing framework (#7)
    adding travis

---
+++ src/lib/config/navStatus.ts
@@ -0,0 +1,18 @@
+module.exports = [
+	"Under way using engine",
+	"At anchor",
+	"Not under command",
+	"Restricted manoeuverability",
+	"Constrained by her draught",
+	"Moored",
+	"Aground",
+	"Engaged in Fishing",
+	"Under way sailing",
+	"Reserved for future amendment of Navigational Status for HSC",
+	"Reserved for future amendment of Navigational Status for WIG",
+	"Reserved for future use",
+	"Reserved for future use",
+	"Reserved for future use",
+	"AIS-SART is active",
+	"Not defined (default)"
+];
---
+++ src/lib/config/stationIntervals.ts
@@ -0,0 +1,18 @@
+module.exports = [
+	'As given by the autonomous mode',
+	'10 Minutes',
+	'6 Minutes',
+	'3 Minutes',
+	'1 Minute',
+	'30 Seconds',
+	'15 Seconds',
+	'10 Seconds',
+	'5 Seconds',
+	'Next Shorter Reporting Interval',
+	'Next Longer Reporting Interval',
+	'Reserved for future use',
+	'Reserved for future use',
+	'Reserved for future use',
+	'Reserved for future use',
+	'Reserved for future use'
+];
