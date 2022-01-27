480ebf4178f1011af31203c87f9d510b6c3a9a3b
Anthony Paul <anthonypaul@gmail.com>
2022-01-27 14:41:00 -0330

    removed commentted lines

--- build.js
+++ build.js
@@ -1,13 +1,3 @@
-const fs = require('fs')
+const fs = require('fs');
 
-fs.rm("dist", { recursive:true }, (err) => {
-    if(err){
-        // File deletion failed
-        // console.error(err.message);
-        return;
-    }
-    // console.log("File deleted successfully");
-      
-    // List files after deleting
-    // getCurrentFilenames();
-})
+fs.rm("dist", { recursive:true }, (err) => { });
